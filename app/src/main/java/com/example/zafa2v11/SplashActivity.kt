ce542d201fe33d9b29453f0f18f8ce64fe0a6680
Billy <Billyflin>
2022-01-12 02:31:13 -0300

    Initial commit

---
+++ app/src/main/java/com/example/zafa2v11/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.example.zafa2v11
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+
+
+}
