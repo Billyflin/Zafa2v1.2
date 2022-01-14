cf9b7317d27c936b60f8247c6d3ff0dabee94fdc
Billy <Billyflin>
2022-01-14 04:24:15 -0300

    af

--- app/src/main/java/com/example/zafa2v11/MainActivity.kt
+++ app/src/main/java/com/example/zafa2v11/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun funciona() {
-        startActivity( Intent(this,QRActivity::class.java))
+        startActivity( Intent(this,MenuActivity::class.java))
     }
 
     private fun registerView() {
--- app/src/main/java/com/example/zafa2v11/MenuActivity.kt
+++ app/src/main/java/com/example/zafa2v11/MenuActivity.kt
@@ -2,10 +2,23 @@ package com.example.zafa2v11
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class MenuActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+        setSupportActionBar(BottomAppBarMenu)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.bottom_app_menu,menu)
+        return true;
+
     }
 }
