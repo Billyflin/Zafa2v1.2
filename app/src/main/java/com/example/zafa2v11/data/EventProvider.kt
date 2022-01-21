268f78f08877d340d4e237fde3a44fed77c14a0a
Billy <Billyflin>
2022-01-20 22:27:02 -0300

    Bl

--- app/src/main/java/com/example/zafa2v11/data/EventProvider.kt
+++ app/src/main/java/com/example/zafa2v11/data/EventProvider.kt
@@ -3,9 +3,9 @@ package com.example.zafa2v11.data
 class EventProvider {
     companion object {
         val listaEventos= listOf(
-            EventItem("Zafa2 ","Zafa2","foto zafa2"),
-            EventItem("Pandora","Pandora","https://www.wallpapertip.com/wmimgs/12-122874_eclipse-solar-hd.jpg"),
-            EventItem("Mestizos","Mestizos","foto Mestizos")
+            EventItem("Zafa2 ","Zafa2","https://cdn.discordapp.com/attachments/931337272094244924/932506020209655818/unknown.png"),
+            EventItem("Pandora","Pandora","https://cdn.discordapp.com/attachments/933210790528421918/933220506637828126/Pandora_Logo_Rework.png"),
+            EventItem("Mestizos","Mestizos","https://cdn.discordapp.com/attachments/933210790528421918/933224356673376276/mestizo_logo_2_rework.png")
         )
     }
 
